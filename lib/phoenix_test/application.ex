85ecdf8cfedd1741a376c12296e0582e06b126a5
dmitryure <dmitryure@gmail.com>
2020-04-18 16:36:39 +0300

    initial

---
+++ lib/phoenix_test/application.ex
@@ -0,0 +1,31 @@
+defmodule PhoenixTest.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the Ecto repository
+      PhoenixTest.Repo,
+      # Start the endpoint when the application starts
+      PhoenixTestWeb.Endpoint
+      # Starts a worker by calling: PhoenixTest.Worker.start_link(arg)
+      # {PhoenixTest.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PhoenixTest.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    PhoenixTestWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
